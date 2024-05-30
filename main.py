ee24eac2202c9bb1a1f4804407e05f397cc681ca
divyam-kalwar <divyamkalwar98@gmail.com>
2024-05-30 22:16:26 +0530

    feat : added wish_me

--- main.py
+++ main.py
@@ -39,18 +39,40 @@ def takecommand():
             return "None"
         return query
 
+def wish_me():
+    hour = (datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good morning Divyam. How are you doing?")
+    elif hour>=12 and hour<18:
+        speak("Good afternoon Divyam. How are you doing?")
+    else:
+        speak("Good evening Divyam. How are you doing?")
+        
+    speak("I am FRIDAY. Tell me Divyam how can I help you?")
+    
 
 if __name__ == "__main__":
-    query = takecommand().lower()
+    while True:
+        wish_me()
+        
+        query = takecommand().lower()
+        browser_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+        
+        if 'wikipedia' in query:
+            speak('Searching wikipedia')
+            query = query.replace('wikipedia','')
+            results = wikipedia.summary(query, sentences =2)
+            speak('According to wikipedia')
+            print(results)
+            speak(results)
 
-    if 'wikipedia' in query:
-        speak('Searching wikipedia')
-        query = query.replace('wikipedia','')
-        results = wikipedia.summary(query, sentences =2)
-        speak('According to wikipedia')
-        print(results)
-        speak(results)
-
-    elif "youtube" in query:
-        speak('Opening Youtube')
-        webbrowser.open('youtube.com')
+        elif "youtube" in query:
+            speak('Opening Youtube')
+            webbrowser.get(using=browser_path).open('youtube.com')
+            
+        elif "google" in query:
+            speak('Opening Google')
+            webbrowser.get(using=browser_path).open('google.com')
+        
+        elif "goodbye" in query:
+            exit()
